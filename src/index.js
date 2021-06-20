5fdeaf63b5e5b97c30410705525a571ca9916958
areenjaradat <areenjaradat96@gmail.com>
2021-06-20 16:36:57 +0300

    try depolying1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom';
 
 import App from './app.js';
 
-const Main = () => <App />;
+class Main extends React.Component {
+  render() {
+    return <App />;
+  }
+}
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<Main />, rootElement);
